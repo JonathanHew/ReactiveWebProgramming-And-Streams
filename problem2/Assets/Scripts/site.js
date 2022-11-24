59d4a77249a29b0180df3b55a9b609a1ed4fa18d
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-24 16:13:10 +0000

    inputted elements captured in JS

---
+++ problem2/Assets/Scripts/site.js
@@ -0,0 +1,5 @@
+const hoursInput = document.getElementById("hoursInput");
+const minutesInput = document.getElementById("minutesInput");
+const secondsInput = document.getElementById("secondsInput");
+const startBtn = document.getElementById("startBtn");
+
