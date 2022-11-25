2308a9f58c054f17d30222ca1383a27d5ad049e4
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-25 14:21:43 +0000

    added max and min values for input, JS to display inputted values on the DOM

--- problem2/Assets/Scripts/site.js
+++ problem2/Assets/Scripts/site.js
@@ -1,5 +1,36 @@
-const hoursInput = document.getElementById("hoursInput");
-const minutesInput = document.getElementById("minutesInput");
-const secondsInput = document.getElementById("secondsInput");
-const startBtn = document.getElementById("startBtn");
+let hoursInput = document.getElementById("hoursInput");
+let minutesInput = document.getElementById("minutesInput");
+let secInput = document.getElementById("secInput");
+let startBtn = document.getElementById("startBtn");
+let timerSection = document.getElementById("timerSection");
 
+
+let newHour = document.createElement("p");
+let newMin = document.createElement("p");
+let newSec = document.createElement("p");
+let colon1 = document.createElement("p");
+let colon2 = document.createElement("p");
+
+let timerValidation = () => {
+    startCountdown();
+
+}
+
+
+let startCountdown = () => {
+    newHour.innerHTML = hoursInput.value;
+    timerSection.appendChild(newHour);
+
+    colon1.innerHTML = ":";
+    timerSection.appendChild(colon1);
+
+    newMin.innerHTML = minutesInput.value;
+    timerSection.appendChild(newMin);
+
+    colon2.innerHTML = ":";
+    timerSection.appendChild(colon2);
+
+    newSec.innerHTML = secInput.value;
+    timerSection.appendChild(newSec);
+
+}
