e37b4ac545df958639b6755925cb8341a7409fbf
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-27 10:59:31 +0000

    rxjs script src added, new js file created

---
+++ problem1/Assets/Scripts/site2.js
@@ -0,0 +1,9 @@
+const { fromEvent, takeWhile } = rxjs;
+let form = document.getElementById("form");
+let titleInput = document.getElementById("titleInput");
+let msg = document.getElementById("msg");
+let bodyInput = document.getElementById("bodyInput");
+let colorInput = document.getElementById("colorInput");
+let notes = document.getElementById("notes");
+let addBtn = document.getElementById("addBtn");
+
