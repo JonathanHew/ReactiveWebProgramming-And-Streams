ea4a86fbc63b07c367cbd30ae7508be3eaea414d
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-27 12:44:58 +0000

    form validation and acceptData from old js added,

--- problem1/Assets/Scripts/site.js
+++ problem1/Assets/Scripts/site.js
@@ -2,7 +2,7 @@ const {Observable,fromEvent}= rxjs;
 
 let form = document.querySelector("form");
 let titleInput = document.getElementById("titleInput");
-let msg = document.getElementById("msg");
+let errMsg = document.getElementById("errMsg");
 let bodyInput = document.getElementById("bodyInput");
 let colorInput = document.getElementById("colorInput");
 let notes = document.getElementById("notes");
@@ -11,6 +11,30 @@ let addBtn = document.getElementById("addBtn");
 fromEvent(form,'submit').subscribe( (e) => {
     e.preventDefault();
     console.log("subcribe working");
-    //formValidation();
+    formValidation();
 });
 
+let formValidation = () =>{
+    if(titleInput.value === "")
+    {
+        console.log("Failure!!");
+        errMsg.innerHTML = "Title cannot be blank";
+    }
+    else
+    {
+        console.log("Success!!");
+        errMsg.innerHTML = "";
+        acceptData();
+    }
+};
+
+let data = {};
+
+let acceptData = () => {
+    data["title"] = titleInput.value;
+    data["body"] = bodyInput.value;
+    data["color"] = colorInput.value;
+
+    console.log(data)
+    //createNotes();
+};
