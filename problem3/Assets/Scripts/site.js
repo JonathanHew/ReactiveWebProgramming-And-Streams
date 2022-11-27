0ba5b3013c8b7b36c82860253f4bb95ff3987c0b
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-27 14:34:57 +0000

    form from previous question added

--- problem3/Assets/Scripts/site.js
+++ problem3/Assets/Scripts/site.js
@@ -1,3 +1,11 @@
+const {Observable,fromEvent}= rxjs;
+
+let form = document.querySelector("form");
+let errMsg = document.getElementById("errMsg");
+let bodyInput = document.getElementById("bodyInput");
+let colorInput = document.getElementById("colorInput");
+let notes = document.getElementById("notes");
+
 class Note extends HTMLElement {
     constructor(parentId, id, body) {
         super();
@@ -6,3 +14,9 @@ class Note extends HTMLElement {
         this.body = body;
       }
 }
+
+fromEvent(form,'submit').subscribe( (e) => {
+    e.preventDefault();
+    console.log("subcribe working");
+    //formValidation();
+});
