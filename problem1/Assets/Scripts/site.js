35efb8de3c8c21b94d7b35bdad8386881670578b
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-11-27 12:39:10 +0000

    subcribe working for form onclick

--- problem1/Assets/Scripts/site.js
+++ problem1/Assets/Scripts/site.js
@@ -1,7 +1,16 @@
-let form = document.getElementById("form");
+const {Observable,fromEvent}= rxjs;
+
+let form = document.querySelector("form");
 let titleInput = document.getElementById("titleInput");
 let msg = document.getElementById("msg");
 let bodyInput = document.getElementById("bodyInput");
 let colorInput = document.getElementById("colorInput");
 let notes = document.getElementById("notes");
 let addBtn = document.getElementById("addBtn");
+
+fromEvent(form,'submit').subscribe( (e) => {
+    e.preventDefault();
+    console.log("subcribe working");
+    //formValidation();
+});
+
