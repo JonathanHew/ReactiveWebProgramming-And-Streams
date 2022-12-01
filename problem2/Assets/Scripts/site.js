be538cceff50b87991e0e570f2067df39dd8f385
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-12-01 14:56:29 +0000

    getting inputted hours mins and seconds from html into JS

--- problem2/Assets/Scripts/site.js
+++ problem2/Assets/Scripts/site.js
@@ -1,3 +1,8 @@
 // Get startBtn and add a observable to it to monitor when it clicks 
 const startBtn = document.querySelector("#startBtn");
 const startObs = Rx.Observable.fromEvent(startBtn, 'click');
+
+// getting the inputted hours, minutes and seconds
+const hoursInput = document.querySelector('#hoursInput');
+const minsInput = document.querySelector('#minsInput');
+const secsInput = document.querySelector('#secsInput');
