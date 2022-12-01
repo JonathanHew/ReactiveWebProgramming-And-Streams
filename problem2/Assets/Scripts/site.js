abf5ce5393ea2cbe065fb5f696b2f8d8dc277f86
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-12-01 14:51:21 +0000

    added countdown section HTML

--- problem2/Assets/Scripts/site.js
+++ problem2/Assets/Scripts/site.js
@@ -1,38 +1,7 @@
-let hoursInput = document.getElementById("hoursInput");
-let minutesInput = document.getElementById("minutesInput");
 let secInput = document.getElementById("secInput");
+const form = document.querySelector("form");
 let startBtn = document.getElementById("startBtn");
 let timerSection = document.getElementById("timerSection");
+let num = document.getElementById("num");
 
 
-let newHour = document.createElement("p");
-let newMin = document.createElement("p");
-let newSec = document.createElement("p");
-let colon1 = document.createElement("p");
-let colon2 = document.createElement("p");
-
-const {Observable,fromEvent} = rxjs;
-
-let timerValidation = () => {
-    startCountdown();
-
-}
-
-
-let startCountdown = () => {
-    newHour.innerHTML = hoursInput.value;
-    timerSection.appendChild(newHour);
-
-    colon1.innerHTML = ":";
-    timerSection.appendChild(colon1);
-
-    newMin.innerHTML = minutesInput.value;
-    timerSection.appendChild(newMin);
-
-    colon2.innerHTML = ":";
-    timerSection.appendChild(colon2);
-
-    newSec.innerHTML = secInput.value;
-    timerSection.appendChild(newSec);
-
-}
